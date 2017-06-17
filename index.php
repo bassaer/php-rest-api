e71ab47f06876fa3971c123ead9e9c5205cf5db3
Tsubasa Nakayama <app.nakayama@gmail.com>
2017-06-18 06:03:12 +0900

    Add scratch api

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require_once(dirname(__FILE__).'/../config.php');    
+$host = $db['host'];
+$user = $db['user'];
+$pass = $db['pass'];
+
+try {
+    $pdo = new PDO($host,$user,$pass);
+    //array(PDO:: ATTR_EMULATE_PREPARES => false)); 
+    $sth = $pdo->prepare("SELECT * FROM articles");
+    $sth->execute();
+    $article = array();
+    while($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+        $article[] = array (
+            'id'=>$row['id'],
+            'title'=>$row['title'],
+            'url'=>$row['url'],
+            'date'=>$row['date']
+        );
+    }
+    header('Content-type: application/json');
+    echo json_encode($article);
+}catch (PDOException $e) { 
+	echo "Error: ".$e->getMessage();
+}
+?>
